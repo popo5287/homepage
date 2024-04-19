47537c1c8a0e761e3dc608ad61df9824a1dfa33e
popo5287 <popo5287@gmail.com>
2024-04-19 10:30:23 +0900

    modal_popup_js commit

--- js/modal.js
+++ js/modal.js
@@ -8,9 +8,39 @@ document.addEventListener('DOMContentLoaded', function() {
       var modal = document.querySelector(target);
       if (modal) {
         modal.style.display = 'block';
+        loadModalContent(modal); // Pass modal element to load modal content
       }
     });
   });
+
+  // Function to load modal content dynamically
+  function loadModalContent(modal) {
+    var modalBody = modal.querySelector('.modal-body');
+    if (modalBody) {
+      var modalId = modal.getAttribute('id'); // Get modal ID
+      var index = modalId.replace('modalContent', ''); // Extract index from modal ID
+      var type = getTypeFromModalId(modalId); // Extract type from modal ID
+      fetch(`../link/${type}_${index}.html`) // Construct link format using type and index
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('네트워크 오류: ' + response.status);
+          }
+          return response.text();
+        })
+        .then(data => {
+          modalBody.innerHTML = data;
+        })
+        .catch(error => {
+          console.error('모달 내용을 가져오는 동안 오류가 발생했습니다.', error);
+        });
+    }
+  }
+
+  // Function to extract type from modal ID
+  function getTypeFromModalId(modalId) {
+    var type = modalId.split('_')[0];
+    return type.replace('modalContent', '');
+  }
 
   var modalCloseButtons = document.querySelectorAll('[data-dismiss="modal"]');
 
@@ -32,44 +62,4 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
-
-  // 이미지 클릭 시 모달 열기
-  var openModalImg = document.querySelector('.thumbs__item[data-toggle="modal"]');
-  var modal = document.getElementById('modalContent');
-  var modalBody = document.getElementById('modalBody');
-  var modalClose = document.querySelector('#modalContent .close');
-
-  if (openModalImg && modal && modalBody && modalClose) {
-    openModalImg.addEventListener('click', function() {
-      modal.style.display = 'block';
-      // 모달 내용 가져오기
-      fetch('link/2404_event.html')
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('네트워크 오류: ' + response.status);
-          }
-          return response.text();
-        })
-        .then(data => {
-          modalBody.innerHTML = data;
-        })
-        .catch(error => {
-          console.error('모달 내용을 가져오는 동안 오류가 발생했습니다.', error);
-        });
-    });
-
-    // 모달 닫기 버튼 클릭 시 모달 닫기
-    modalClose.addEventListener('click', function() {
-      modal.style.display = 'none';
-    });
-
-    // 모달 외부 클릭 시 모달 닫기
-    window.addEventListener('click', function(event) {
-      if (event.target === modal) {
-        modal.style.display = 'none';
-      }
-    });
-  } else {
-    console.error('요소들을 찾을 수 없습니다.');
-  }
 });
